01f1d9dbaf2f34425b03291b9af9f30756880573
Mário Vidal <amjvl@discente.ifpe.edu.br>
2026-09-18 01:52:16 -0300

    Criando Decorator e StrawberryCake

---
+++ bakery/bakery/CakeDecorator.java
@@ -0,0 +1,14 @@
+public abstract class CakeDecorator extends Cake {
+    protected Cake cake;
+    public CakeDecorator(Cake cake) {
+        this.cake = cake;
+    }
+    @Override
+    public int getCost() {
+        return cake.getCost();
+    }
+    @Override
+    public String getDescription() {
+        return cake.getDescription();
+    }
+}
