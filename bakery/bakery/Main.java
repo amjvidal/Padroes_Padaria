868883e935424cb921f32a801877e3ec2da6d98d
Mário Vidal <amjvl@discente.ifpe.edu.br>
2026-09-18 01:55:55 -0300

    Atualizando a Main

--- bakery/bakery/Main.java
+++ bakery/bakery/Main.java
@@ -3,12 +3,25 @@
  */
 public class Main {
     public static void main(String[] args) {
-        // Create the order
         Order order = new Order();
-        order.addCake(new VanillaCake());
+        
         order.addCake(new ChocolateCake());
-
-        // Print the order
+        
+        order.addCake(new Saying(new VanillaCake(), "PLAIN!"));
+        
+        order.addCake(new Saying(new Sprinkles(new VanillaCake()), "FANCY!"));
+        
+        order.addCake(
+            new Saying(
+                new Saying(
+                    new Sprinkles(
+                        new Sprinkles(
+                            new MultiLayered(new StrawberryCake())
+                        )
+                    ), "One of"
+                ), "EVERYTHING"
+            )
+        );
         order.printOrder();
     }
 }
